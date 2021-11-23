48052382e6cddd7fa40b75b7add2a7baad0c8e86
vaibhavkale <vailegend@gmail.com>
2021-11-23 23:47:43 +0530

    Bug fix module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,7 +9,10 @@ class QuizQuestion extends Component {
   }
   handleClick(buttonText) {
     if(buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false })
       this.props.showNextQuestionHandler()
+    } else {
+      this.setState({ incorrectAnswer: true })
     }
   }
   render() {
